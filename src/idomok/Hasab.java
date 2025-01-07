c1b4b0a0929d046889e9993528ef80ece3976891
zltnszekeres <zltn.szekeres@yahoo.com>
2025-01-07 12:37:36 +0100

    szerkezet

---
+++ src/idomok/Hasab.java
@@ -0,0 +1,7 @@
+
+package idomok;
+
+
+public class Hasab {
+    
+}
---
+++ src/idomok/Idomok.java
@@ -0,0 +1,11 @@
+
+package idomok;
+
+
+public class Idomok {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
