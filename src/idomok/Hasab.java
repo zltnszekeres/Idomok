2a70ad8ead38351ef30ccf91032b30c298793a23
zltnszekeres <zltn.szekeres@yahoo.com>
2025-01-07 12:48:59 +0100

    hasab

--- src/idomok/Hasab.java
+++ src/idomok/Hasab.java
@@ -2,6 +2,28 @@
 package idomok;
 
 
-public class Hasab {
-    
+public class Hasab extends Idom {
+
+    double a, b, m;
+
+    public Hasab(double a, double b, double m) {
+        this.a = a;
+        this.b = b;
+        this.m = m;
+    }
+
+    @Override
+    public double terfogat() {
+        return a * b * m;
+    }
+
+    double getAlapterulet() {
+        return a * b;
+    }
+
+    @Override
+    public String toString() {
+        return "Hasáb {" + super.toString() + " a=" + a + " b=" + b + " m=" + m + "}";
+    }
+
 }
