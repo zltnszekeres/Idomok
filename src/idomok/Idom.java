44dfaac73e54eb56d6fdbe66d2fd9945a08de2dd
zltnszekeres <zltn.szekeres@yahoo.com>
2025-01-07 12:44:35 +0100

    idom

--- src/idomok/Idom.java
+++ src/idomok/Idom.java
@@ -2,6 +2,19 @@
 package idomok;
 
 
-public class Idom {
+abstract class Idom {
+    
+    private static double fajsuly = 0.8;
+
+    abstract double terfogat();
+
+    double suly() {
+        return fajsuly * terfogat();
+    }
+
+    @Override
+    public String toString() {
+        return "fajsuly= " + fajsuly + " terfogat= " + terfogat() + " súly= " + suly() + "}";
+    }
     
 }
